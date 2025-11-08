b94f53abcb849f964fb64c79e8e3cdc07156ed51
Pavithraa-08 <pavithraagangadhar@gmail.com>
2025-11-08 16:20:04 +0000

    initial r code

---
+++ week 5 assignment/consistency_checker(1).R
@@ -0,0 +1,37 @@
+# Change this to TRUE if you want to force a reinstall of SCEMChecker
+# (Useful to get the latest version, as this is still a work in progress)
+reinstall_SCEMchecker <- TRUE
+
+# ========== SETUP ==========
+if(!("remotes") %in% rownames(installed.packages())){
+  install.packages("remotes", repos = "https://cloud.r-project.org")
+}
+
+if(reinstall_SCEMchecker | !("SCEMChecker") %in% rownames(installed.packages())){
+  remotes::install_github("fcampelo/SCEMChecker",
+                          dependencies = c("Imports"),
+                          force = TRUE)
+}
+
+library(SCEMChecker)
+
+
+# ========== SET CHECKING PARAMETERS ==========
+
+# Path to your submission file (change as needed)
+submission_path <- "./W05_Assignment_AnswerTemplate.Rmd"
+
+# Change lab number to the one you want to check
+lab_number <- 5
+
+
+# ========== RUN CHECKER ==========
+
+# 'mycheck' will contain a data frame with the details of the check
+mycheck <- consistency_checker(submission_path = submission_path, template_number = lab_number)
+
+print(mycheck) # <--- uncomment to inspect the raw data frame
+
+# This prints a summary of any issues encountered
+# If there are no issues, it shows a blank list
+summary(mycheck)
